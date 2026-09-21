9b3c68710c67ea6ba5bf2e06ffb4a2da6602e07b
VitorAndreSilva <alunoitorandre@gmail.com>
2026-09-21 13:51:16 -0300

    fix: update banner image URL

--- src/modules/blog/components/BlogHero.vue
+++ src/modules/blog/components/BlogHero.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const urlBanner =
-  'https://api-aqua.michalski.app/media/images/4bb61b9a-b42d-4c9e-9622-7ca8a1cd53df.png'
+  'https://api-aqua.michalski.app/media/images/31063604-280d-4143-a9d0-25dd7ec1b904.png'
 </script>
 
 <template>
